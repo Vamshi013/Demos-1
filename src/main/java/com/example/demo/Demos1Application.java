030634ad64791d64e6e7e637b2c1e6e72237357c
Vamshi013 <sprintbootraining@gmail.com>
2022-01-24 16:00:06 +0530

    First Commit

---
+++ src/main/java/com/example/demo/Demos1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demos1Application.class, args);
+	}
+
+}
