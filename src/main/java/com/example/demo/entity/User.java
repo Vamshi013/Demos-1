660c3d87c722b72fa3529ee2bb49986ef502d52d
Vamshi013 <sprintbootraining@gmail.com>
2022-01-25 00:22:57 +0530

    Blank Changes

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class User {
@@ -18,7 +19,7 @@ public class User {
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-
+	@NotBlank
 	private String name;
 	public Integer getId() {
 		return id;
